8c063b8e0996da3dc636e2a020d761e86e09d795
athumaniMfaume <athumanimfaume1995@gmail.com>
2026-06-14 14:52:12 +0300

    set env for neon database:

--- config/database.php
+++ config/database.php
@@ -82,20 +82,24 @@ return [
             ]) : [],
         ],
 
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => env('DB_CHARSET', 'utf8'),
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'search_path' => 'public',
-            'sslmode' => 'prefer',
-        ],
+'pgsql' => [
+    'driver' => 'pgsql',
+    'url' => env('DB_URL'), // Keep this, it's perfect
+    'host' => env('DB_HOST', '127.0.0.1'),
+    'port' => env('DB_PORT', '5432'),
+    'database' => env('DB_DATABASE', 'laravel'),
+    'username' => env('DB_USERNAME', 'root'),
+    'password' => env('DB_PASSWORD', ''),
+    'charset' => env('DB_CHARSET', 'utf8'),
+    'prefix' => '',
+    'prefix_indexes' => true,
+    'search_path' => 'public',
+    'sslmode' => env('DB_SSLMODE', 'require'), // Set default to 'require' for Neon
+    'options' => [
+        PDO::ATTR_EMULATE_PREPARES => true, // Add this for Neon pooling
+    ],
+],
+
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
